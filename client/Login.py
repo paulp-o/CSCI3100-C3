e1786052b2597c47a7c505d14eef2010b56a5a20
Aaron Park <1155167933@link.cuhk.edu.hk>
2024-04-10 01:48:14 +0800

    new login

--- client/Login.py
+++ client/Login.py
@@ -2,111 +2,52 @@ import pygame, sys
 import Game
 import Button
 
-button_blue = pygame.transform.scale(pygame.image.load('Assets/Settings/settingbutton_blue.png'), (320, 60))
-
-global base_font
-global user_id
-global user_password
-global id_rect
-global password_rect
-global color_active
-global color_passive
-global color_id
-global  color_password
-global active_id
-global active_password
-
 
 class Login:
     def __init__(self, display, gameStateManager):
         self.display = display
         self.gameStateManager = gameStateManager
-        self.background = pygame.image.load('Assets/Main Menu/background.jpg')
-        
-        title_font = pygame.font.Font(None, 120)  
+        text_font = pygame.font.Font(None,32)
+        title_font = pygame.font.Font(None, 60)  
 
-        self.text_login = title_font.render('Login', False, 'white')
-        
-        self.text_verify = base_font.render('Register', False, 'white')
-        self.verify_button = Button.Button(330, 180, button_blue, 1, self.try_login)
-        
-        base_font = pygame.font.Font(None,32)
-        user_id = ''
-        user_password = ''
-        id_rect = pygame.Rect(330, 90, 140, 32)
-        password_rect = pygame.Rect(330, 130, 140, 32)
+        # Icon
+        self.login_icon = pygame.transform.scale(pygame.image.load("Assets/Login/login_icon.png").convert_alpha(), (6000, 1500))
 
-        color_active = pygame.Color("white")
-        color_passive = pygame.Color("gray15")
-        color_id = color_passive
-        color_password = color_passive
-
-        active_id = False
-        active_password = False
-             
+        
+        # Text
+        self.text_title = title_font.render('Login', False, 'white')
+        self.text_id = text_font.render('your id', False, 'white')
+        self.text_password = text_font.render('your password', False, 'white')
+        
+        # Button
+        self.LOGIN_BUTTON_POS = (50,300)
+        self.login_button = Button.OptionButton(self.LOGIN_BUTTON_POS[0],
+                                                self.LOGIN_BUTTON_POS[1],
+                                                self.login_icon,
+                                                0.05,
+                                                self.try_login,
+                                                )
+        
                 
     def run(self):
-         
-        Game.screen.blit(self.background, (0, 0))
-        Game.screen.blit(self.text_login, (330, 30))
+        Game.screen.fill((200, 200, 180))
         
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-                
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    if id_rect.collidepoint(event.pos):
-                        active_id = True
-                    else:
-                        active_id = False
-                    
-                    if password_rect.collidepoint(event.pos):
-                        active_password = True
-                    else:
-                        active_password = False
-                    
-                    if event.type == pygame.KEYDOWN:
-                        if active_id == True:
-                            if event.key == pygame.K_BACKSPACE:
-                                user_id = user_id[0:-1]
-                            else:
-                                user_id += event.unicode
-                        elif active_password == True:
-                            if event.key == pygame.K_BACKSPACE:
-                                user_password = user_id[0:-1]
-                            else:
-                                user_password += event.unicode
-            if active_id:
-                color_id = color_active
-            else:
-                color_id = color_passive
-            if active_password:
-                color_password = color_active
-            else:
-                color_password = color_passive 
-            
-            Game.screen.blit(id_rect, (330, 90))
-            Game.screen.blit(password_rect, (330, 130))
-            
-            id_surface = base_font.render(user_id,True,(255,255,255))
-            password_surface = base_font.render(user_password,True,(255,255,255))
-            
-            Game.screen.blit(id_surface, (id_rect.x + 5, id_rect.y + 5))
-            Game.screen.blit(password_surface, (password_rect.x + 5, password_rect.y + 5))
-            
-            id_rect.w = max (100, id_surface.get_width()+10)
-            password_rect.w = max (100, password_surface.get_width()+10)
-
-            pygame.display.flip()
+        # Text
+        Game.screen.blit(self.text_title, (50,50))
+        Game.screen.blit(self.text_id, (50, 150))
+        Game.screen.blit(self.text_password, (50,200))
         
-        if self.verify_button.back():
-            self.verify_button.action()
+        # Button
+        self.login_button.draw()
+        
         
     
     def try_login(self):
         #need verify procedure
-        if True:
-            self.gameStateManager.set_state('main')
+        self.verify = True
+        if self.verify:
+            self.gameStateManager.set_state('main') # go to main menu
             
+        
+        
+       
