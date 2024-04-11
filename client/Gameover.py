23d6ca921f0aa173b5c80f95f2926e2aeb4a881d
Aaron Park <1155167933@link.cuhk.edu.hk>
2024-04-11 08:01:25 +0800

    update back button

--- client/Gameover.py
+++ client/Gameover.py
@@ -1,18 +1,34 @@
 import pygame
 import Game
+import Button
 
 
 class Gameover:
     def __init__(self, display, gameStateManager):
         self.display = display
         self.gameStateManager = gameStateManager
-        self.score_text_font = pygame.font.SysFont("comicsansms", 70)
+        title_font = pygame.font.SysFont("comicsansms", 70)
+        text_font = pygame.font.SysFont("comicsansms", 35)
 
-        self.score_text = self.score_text_font.render('Game Over', False, 'white')
-        self.score_text_rect = self.score_text.get_rect(
-        # to ensure center of text at top
-        center=(Game.SCREEN_WIDTH/2, 50))
+         # Icon / Image
+        self.back_icon = pygame.image.load("Assets/Settings/return.png").convert_alpha()
+        self.back_button = Button.Button(Game.SCREEN_WIDTH - 60, 5, self.back_icon, 0.5)
+        self.back_button.action = lambda: self.gameStateManager.set_state('main')
+        
+        # title
+        self.rect_title = pygame.Rect((Game.SCREEN_WIDTH)/2-100, 100, 200, 40)
+        self.text_title = title_font.render('Game Over', False, 'white')
+        self.text_title_rect = self.text_title.get_rect()
+        self.text_title_rect.center = self.rect_title.center
+        
+        
 
     def run(self):
         Game.screen.fill((200, 200, 180))  # giving background color
-        Game.screen.blit(self.score_text, self.score_text_rect)
+        
+        self.back_button.draw()
+        
+        Game.screen.blit(self.text_title, self.text_title_rect)
+        
+        
+        
